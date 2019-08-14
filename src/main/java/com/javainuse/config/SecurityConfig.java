6b5065f070b9a4ddd0b459c7dbb0a68619ea94fe
dusitsiri <dusit.siri12@gmail.com>
2019-08-14 11:55:08 +0700

    edit /login until success

--- src/main/java/com/javainuse/config/SecurityConfig.java
+++ src/main/java/com/javainuse/config/SecurityConfig.java
@@ -1,10 +1,13 @@
 package com.javainuse.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
@@ -18,12 +21,25 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Override
 	public void configure(HttpSecurity http) throws Exception {
-		http.antMatcher("/**").authorizeRequests().anyRequest().hasRole("USER")
-				.and().formLogin().loginPage("/login.jsp")
-				.failureUrl("/login.jsp?error=1").loginProcessingUrl("/login")
-				.permitAll().and().logout()
-				.logoutSuccessUrl("/listEmployees.html");
-
+		http.antMatcher("/**")
+			.authorizeRequests()
+				.anyRequest().hasRole("USER")
+				.and()
+					.formLogin()
+					.loginPage("/login.jsp")
+					.failureUrl("/login.jsp?error=1")
+					.loginProcessingUrl("/login")
+					.defaultSuccessUrl("/listEmployees.html")
+					.permitAll()
+				.and()
+					.logout()
+					.logoutUrl("/logout")
+					.deleteCookies("JSESSIONID");
+	}
+	
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
 	}
 
 }
